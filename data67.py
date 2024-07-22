614f0b8f45b5786de6ce49716c1ade0f3126e075
Ajiu0519 <149450010+Ajiu0519@users.noreply.github.com>
2024-07-22 10:43:31 +0800

    Update data67.py

--- data67.py
+++ data67.py
@@ -11,8 +11,8 @@ from vega_datasets import data
 st.title('六七月数据报表')
 
 @st.cache
-all_data = pd.read_csv("data/非标六七月汇总 (2).csv")
-df = pd.DataFrame(all_data)
+df = pd.read_csv("data/非标六七月汇总 (2).csv", encoding='utf-8')
+#df = pd.DataFrame(all_data)
 df = df.drop('偷子', axis=1) #由于偷子列暂时没有数据，所以先删除掉
 
 df.head(20)
