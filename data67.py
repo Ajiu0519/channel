e76182b5edb18e486249633275d0ad4b32ba3175
Ajiu0519 <149450010+Ajiu0519@users.noreply.github.com>
2024-07-22 11:04:01 +0800

    Update data67.py

--- data67.py
+++ data67.py
@@ -11,8 +11,8 @@ import altair as alt
 st.title('六七月数据报表')
 
 @st.cache
-all_data = pd.read_csv('jetbrains://pycharm/navigate/reference?project=channel&path=data/非标六七月汇总 (2).csv')
-df = pd.DataFrame(all_data)
+data = pd.read_csv("非标六七月汇总 (3).csv", encoding = 'utf-8')
+df = pd.DataFrame(data)
 df = df.drop('偷子', axis=1) #由于偷子列暂时没有数据，所以先删除掉
 
 df.head(20)
